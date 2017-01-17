b1f95a081b07efc47755dada83b9bfaffea9214c
aaronrcs <aromer61@calstatela.edu>
2017-01-17 07:37:56 -0800

    Added First C++ File

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CPP
+//
+//  Created by Aaron Romero on 1/17/17.
+//  Copyright © 2017 Aaron Romero. All rights reserved.
+//
+
+//This is my first C++ program, of course I have to stick with the classics and create a "hello World" program
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    std::cout << "This my main.cpp File!\n";
+    std::cout<< "Hello to the World of C++!\n";
+    return 0;
+}

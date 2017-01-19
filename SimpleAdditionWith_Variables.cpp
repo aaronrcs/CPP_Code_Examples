af23a5562475a73dee25ef83f6b17c27b66cfb94
aaronrcs <aromer61@calstatela.edu>
2017-01-19 00:03:10 -0800

    Simple Addition with Variables

---
+++ SimpleAdditionWith_Variables.cpp
@@ -0,0 +1,21 @@
+//
+//  SimpleAdditionWith_Variables.cpp
+//  CPP
+//
+//  Created by Aaron Romero on 1/19/17.
+//  Copyright © 2017 Aaron Romero. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 50;
+    int y = 50;
+    int sum = x + y;
+    
+    cout << sum;
+    
+    return 0;
+}
